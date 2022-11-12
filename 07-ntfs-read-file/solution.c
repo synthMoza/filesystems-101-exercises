927806e42b3fcd33bb80fe82ad472f29ef0af0a1
synthMoza <eganian.aa@phystech.edu>
2022-11-13 02:24:50 +0300

    implement my_ntfs_pathname_to_inode that might return ENOTDIR

--- 07-ntfs-read-file/solution.c
+++ 07-ntfs-read-file/solution.c
@@ -30,6 +30,120 @@ int GetFileNameByFd(int fd, char* buffer)
 	return 0;
 }
 
+/*
+	ntfs_pathname_to_inode returns ENOENT even if it was directory, so rewrite (a bit) to return ENOTDIR if it was a directory
+*/
+ntfs_inode* my_ntfs_pathname_to_inode(ntfs_volume *vol, ntfs_inode *parent, const char *pathname)
+{
+	u64 inum;
+	int len, err = 0;
+	char *p, *q;
+	ntfs_inode *ni;
+	ntfs_inode *result = NULL;
+	ntfschar *unicode = NULL;
+	char *ascii = NULL;
+
+	if (!vol || !pathname) {
+		errno = EINVAL;
+		return NULL;
+	}
+	
+	ntfs_log_trace("path: '%s'\n", pathname);
+	
+	ascii = strdup(pathname);
+	if (!ascii) {
+		ntfs_log_error("Out of memory.\n");
+		err = ENOMEM;
+		goto out;
+	}
+
+	p = ascii;
+	/* Remove leading /'s. */
+	while (p && *p && *p == PATH_SEP)
+		p++;
+
+	if (parent) {
+		ni = parent;
+	} else {
+
+		ni = ntfs_inode_open(vol, FILE_root);
+		if (!ni) {
+			ntfs_log_debug("Couldn't open the inode of the root "
+					"directory.\n");
+			err = EIO;
+			result = (ntfs_inode*)NULL;
+			goto out;
+		}
+	}
+
+	while (p && *p) {
+		/* Find the end of the first token. */
+		q = strchr(p, PATH_SEP);
+		if (q != NULL) {
+			*q = '\0';
+		}
+		len = ntfs_mbstoucs(p, &unicode);
+		if (len < 0) {
+			ntfs_log_perror("Could not convert filename to Unicode:"
+					" '%s'", p);
+			err = errno;
+			goto close;
+		} else if (len > NTFS_MAX_NAME_LEN) {
+			err = ENAMETOOLONG;
+			goto close;
+		}
+		inum = ntfs_inode_lookup_by_name(ni, unicode, len);
+
+		if (inum == (u64) -1) {
+			ntfs_log_debug("Couldn't find name '%s' in pathname "
+					"'%s'.\n", p, pathname);
+			// if it was a directory, we will find another PATH_SEP
+			q = strchr(p, PATH_SEP);
+			if (q)
+				err = ENOTDIR;
+			else
+				err = ENOENT;
+			goto close;
+		}
+
+		if (ni != parent)
+			if (ntfs_inode_close(ni)) {
+				err = errno;
+				goto out;
+			}
+
+		inum = MREF(inum);
+		ni = ntfs_inode_open(vol, inum);
+		if (!ni) {
+			ntfs_log_debug("Cannot open inode %llu: %s.\n",
+					(unsigned long long)inum, p);
+			err = EIO;
+			goto close;
+		}
+	
+		free(unicode);
+		unicode = NULL;
+
+		if (q) *q++ = PATH_SEP; /* JPA */
+		p = q;
+		while (p && *p && *p == PATH_SEP)
+			p++;
+	}
+
+	result = ni;
+	ni = NULL;
+close:
+	if (ni && (ni != parent))
+		if (ntfs_inode_close(ni) && !err)
+			err = errno;
+out:
+	free(ascii);
+	free(unicode);
+	if (err)
+		errno = err;
+	return result;
+}
+
 int dump_file(int img, const char *path, int out)
 {
 	ntfs_attr* attr = NULL;
@@ -54,11 +168,11 @@ int dump_file(int img, const char *path, int out)
 		return -1;
 
 	// find inode by path
-	inode = ntfs_pathname_to_inode(vol, NULL, path);
+	inode = my_ntfs_pathname_to_inode(vol, NULL, path);
 	if (!inode)
 	{
 		r = -errno;
-		goto out;
+		goto main_out;
 	}
 
 	// copy inode data to out fd
@@ -67,7 +181,7 @@ int dump_file(int img, const char *path, int out)
 	if (!attr)
 	{
 		r = -1;
-		goto out;
+		goto main_out;
 	}
 
 	u32 blockSize = 0;
@@ -92,7 +206,7 @@ int dump_file(int img, const char *path, int out)
 		if (bytesRead < 0) 
 		{
 			r = -errno;
-			goto out;
+			goto main_out;
 		}
 
 		if (!bytesRead)
@@ -102,13 +216,13 @@ int dump_file(int img, const char *path, int out)
 		if (written != bytesRead)
 		{
 			r = -errno;
-			goto out;
+			goto main_out;
 		}
 		
 		offset += bytesRead;
 	}
 
-out:
+main_out:
 	ntfs_attr_close(attr);
 	ntfs_inode_close(inode);
 	ntfs_umount(vol, FALSE);
