70de11e1234d4788f25a1f79e5c7fc615d4ead52
synthMoza <eganian.aa@phystech.edu>
2022-12-11 19:03:46 +0300

    Copy 11 solution to 12, add inc

--- 12-grpc-prometheus/pkg/parhash/server.go
+++ 12-grpc-prometheus/pkg/parhash/server.go
@@ -2,10 +2,19 @@ package parhash
 
 import (
 	"context"
+	"log"
+	"net"
+	"sync"
 
 	"github.com/pkg/errors"
 	"github.com/prometheus/client_golang/prometheus"
 	"golang.org/x/sync/semaphore"
+	"google.golang.org/grpc"
+
+	"fs101ex/pkg/workgroup"
+
+	hashpb "fs101ex/pkg/gen/hashsvc"
+	parhashpb "fs101ex/pkg/gen/parhashsvc"
 )
 
 type Config struct {
@@ -54,29 +63,122 @@ type Config struct {
 type Server struct {
 	conf Config
 
+	stop            context.CancelFunc
+	l               net.Listener
+	wg              sync.WaitGroup
+	mutex           sync.Mutex // sync between concurrent ParallelHash() calls
+	globalBufferIdx int        // counter for ignoring order of goroutines
+	callCounter     prometheus.Counter
+
 	sem *semaphore.Weighted
 }
 
 func New(conf Config) *Server {
 	return &Server{
-		conf: conf,
-		sem:  semaphore.NewWeighted(int64(conf.Concurrency)),
+		conf:            conf,
+		sem:             semaphore.NewWeighted(int64(conf.Concurrency)),
+		globalBufferIdx: 0, // default init
 	}
 }
 
 func (s *Server) Start(ctx context.Context) (err error) {
 	defer func() { err = errors.Wrap(err, "Start()") }()
 
-	/* implement me */
+	// ctx with cancel allows to cancel all goroutines in case of error
+	ctx, s.stop = context.WithCancel(ctx)
+
+	s.l, err = net.Listen("tcp", s.conf.ListenAddr)
+	if err != nil {
+		return err
+	}
+
+	s.callCounter = prometheus.NewCounter(
+		prometheus.CounterOpts{
+			Name: "nr_requests",
+		})
+
+	srv := grpc.NewServer()
+	parhashpb.RegisterParallelHashSvcServer(srv, s)
+
+	s.wg.Add(2)
+	go func() {
+		defer s.wg.Done()
+
+		srv.Serve(s.l)
+	}()
+	go func() {
+		defer s.wg.Done()
+
+		<-ctx.Done()
+		s.l.Close()
+	}()
 
 	return nil
 }
 
 func (s *Server) ListenAddr() string {
-	/* implement me */
-	return ""
+	return s.l.Addr().String()
 }
 
 func (s *Server) Stop() {
-	/* implement me */
+	s.stop()
+	s.wg.Wait()
+}
+
+func (s *Server) ParallelHash(ctx context.Context, req *parhashpb.ParHashReq) (resp *parhashpb.ParHashResp, err error) {
+	// record call metric
+	s.callCounter.Inc()
+
+	countBackends := len(s.conf.BackendAddrs)
+	connectionsSlice := make([]*grpc.ClientConn, countBackends)
+	clientsSlice := make([]hashpb.HashSvcClient, countBackends)
+
+	// connect to backends
+	for i := 0; i < countBackends; i++ {
+		connectionsSlice[i], err = grpc.Dial(s.conf.BackendAddrs[i], grpc.WithInsecure())
+		if err != nil {
+			log.Fatalf("Couldn't connect to backend addr %s with error %s", s.conf.BackendAddrs[i], err)
+		}
+
+		defer connectionsSlice[i].Close()
+
+		clientsSlice[i] = hashpb.NewHashSvcClient(connectionsSlice[i])
+	}
+
+	countBuffers := len(req.Data)
+	hashes := make([][]byte, countBuffers)
+
+	// limit goroutines count with semaphore
+	waitGroup := workgroup.New(workgroup.Config{Sem: s.sem})
+	for i := 0; i < countBuffers; i++ {
+		currentBufferIdx := i
+		waitGroup.Go(ctx, func(ctx context.Context) error {
+			s.mutex.Lock()
+			// claim indices under lock
+			currentBackendIdx := s.globalBufferIdx
+			// update global idx in round robin manner
+			s.globalBufferIdx = (s.globalBufferIdx + 1) % countBackends
+
+			hashReq := hashpb.HashReq{Data: req.Data[currentBufferIdx]}
+			s.mutex.Unlock()
+
+			r, err := clientsSlice[currentBackendIdx].Hash(ctx, &hashReq)
+			if err != nil {
+				return err
+			}
+
+			s.mutex.Lock()
+			hashes[currentBufferIdx] = r.Hash
+			s.mutex.Unlock()
+
+			return nil
+		})
+	}
+
+	err = waitGroup.Wait()
+	if err != nil {
+		log.Fatalf("Error: can't hash given data with error %v", err)
+	}
+
+	return &parhashpb.ParHashResp{Hashes: hashes}, nil
 }
